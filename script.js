d93c44d47d597cad9a29bb3f9ff83310f5b52e03
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-06-24 17:15:40 -0600

    fully functioning app with output displayed

--- script.js
+++ script.js
@@ -15,17 +15,45 @@ const dogFoodInfo = [
 
 const cupsPerGallon = 16;
 
-let userInputs = {
-    mealsPerDay: 2,
-    cupsPerMeal: 1
-};
 
-function calculate() {
-    const cupsPerBag = dogFoodInfo.gallonsPerBag * cupsPerGallon;
-    const cupsPerDay = userInputs.mealsPerDay * userInputs.cupsPerMeal;
+function calculate(mealsPerDay, cupsPerMeal, index) {
+    const cupsPerBag = dogFoodInfo[index].gallonsPerBag * cupsPerGallon;
+    const cupsPerDay = mealsPerDay * cupsPerMeal;
     const daysPerBag = cupsPerBag / cupsPerDay;
     return daysPerBag;
 };
+
+function retrieveFormInfo() {    
+    
+    let dogFoodChosen = document.querySelector('input[name="food_type"]:checked');
+
+    let index;
+    switch (dogFoodChosen.value) {
+        case "dog_food_1":
+            index = 0;
+            break;
+        case "dog_food_2":
+            index = 1;
+            break;
+    }
+
+    const userMealsPerDay = Number(document.querySelector('input[name="meals_per_day"]:checked').value);
+
+    const userCupsPerMeal = Number(document.getElementById("cups_per_meal").value);
+
+    const output = calculate(userMealsPerDay, userCupsPerMeal, index);
+
+    document.getElementById("dog_food_weight").innerHTML = dogFoodInfo[index].size;
+    document.getElementById("dog_food_chosen").innerHTML = dogFoodInfo[index].name;
+    document.getElementById("days").innerHTML = output;
+    
+    
+    
+};
+
+
+
+
 
 
 
